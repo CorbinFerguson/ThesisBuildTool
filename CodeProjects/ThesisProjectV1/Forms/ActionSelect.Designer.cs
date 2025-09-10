748b4ced4a3e23c90756a484e6d1bc382da6d46f
corbin.ferguson <corbin.ferguson@endress.com>
2025-09-10 11:38:04 -0400

    added autosize for action select

--- CodeProjects/ThesisProjectV1/Forms/ActionSelect.Designer.cs
+++ CodeProjects/ThesisProjectV1/Forms/ActionSelect.Designer.cs
@@ -37,14 +37,18 @@
             this.SaveButton = new System.Windows.Forms.Button();
             this.LoadButton = new System.Windows.Forms.Button();
             this.NewFile = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // ImportSelect
             // 
-            this.ImportSelect.Location = new System.Drawing.Point(12, 46);
+            this.ImportSelect.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ImportSelect.Location = new System.Drawing.Point(3, 42);
             this.ImportSelect.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.ImportSelect.Name = "ImportSelect";
-            this.ImportSelect.Size = new System.Drawing.Size(107, 80);
+            this.ImportSelect.Size = new System.Drawing.Size(179, 80);
             this.ImportSelect.TabIndex = 0;
             this.ImportSelect.Text = "Import Element";
             this.ImportSelect.UseVisualStyleBackColor = true;
@@ -52,10 +56,12 @@
             // 
             // GenerateSelect
             // 
-            this.GenerateSelect.Location = new System.Drawing.Point(126, 46);
+            this.GenerateSelect.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.GenerateSelect.Location = new System.Drawing.Point(188, 42);
             this.GenerateSelect.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.GenerateSelect.Name = "GenerateSelect";
-            this.GenerateSelect.Size = new System.Drawing.Size(107, 80);
+            this.GenerateSelect.Size = new System.Drawing.Size(182, 80);
             this.GenerateSelect.TabIndex = 1;
             this.GenerateSelect.Text = "Create from Template";
             this.GenerateSelect.UseVisualStyleBackColor = true;
@@ -63,20 +69,25 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(44, 20);
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.SetColumnSpan(this.textBox1, 4);
+            this.textBox1.Location = new System.Drawing.Point(3, 2);
             this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox1.Name = "textBox1";
             this.textBox1.ReadOnly = true;
-            this.textBox1.Size = new System.Drawing.Size(385, 22);
+            this.textBox1.Size = new System.Drawing.Size(743, 22);
             this.textBox1.TabIndex = 2;
             this.textBox1.Text = "Select the action you wish to take:";
             // 
             // ExitButton
             // 
-            this.ExitButton.Location = new System.Drawing.Point(312, 143);
+            this.ExitButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ExitButton.Location = new System.Drawing.Point(563, 144);
             this.ExitButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.ExitButton.Name = "ExitButton";
-            this.ExitButton.Size = new System.Drawing.Size(67, 33);
+            this.ExitButton.Size = new System.Drawing.Size(183, 33);
             this.ExitButton.TabIndex = 3;
             this.ExitButton.Text = "Exit";
             this.ExitButton.UseVisualStyleBackColor = true;
@@ -84,10 +95,12 @@
             // 
             // ModifySelect
             // 
-            this.ModifySelect.Location = new System.Drawing.Point(237, 46);
+            this.ModifySelect.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ModifySelect.Location = new System.Drawing.Point(376, 42);
             this.ModifySelect.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.ModifySelect.Name = "ModifySelect";
-            this.ModifySelect.Size = new System.Drawing.Size(107, 80);
+            this.ModifySelect.Size = new System.Drawing.Size(181, 80);
             this.ModifySelect.TabIndex = 4;
             this.ModifySelect.Text = "Modify Element";
             this.ModifySelect.UseVisualStyleBackColor = true;
@@ -95,10 +108,12 @@
             // 
             // DeleteSelect
             // 
-            this.DeleteSelect.Location = new System.Drawing.Point(349, 46);
+            this.DeleteSelect.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.DeleteSelect.Location = new System.Drawing.Point(563, 42);
             this.DeleteSelect.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.DeleteSelect.Name = "DeleteSelect";
-            this.DeleteSelect.Size = new System.Drawing.Size(107, 80);
+            this.DeleteSelect.Size = new System.Drawing.Size(183, 80);
             this.DeleteSelect.TabIndex = 5;
             this.DeleteSelect.Text = "Delete Element";
             this.DeleteSelect.UseVisualStyleBackColor = true;
@@ -106,10 +121,12 @@
             // 
             // SaveButton
             // 
-            this.SaveButton.Location = new System.Drawing.Point(166, 143);
+            this.SaveButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.SaveButton.Location = new System.Drawing.Point(188, 144);
             this.SaveButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.SaveButton.Name = "SaveButton";
-            this.SaveButton.Size = new System.Drawing.Size(67, 33);
+            this.SaveButton.Size = new System.Drawing.Size(182, 33);
             this.SaveButton.TabIndex = 6;
             this.SaveButton.Text = "Save";
             this.SaveButton.UseVisualStyleBackColor = true;
@@ -117,10 +134,12 @@
             // 
             // LoadButton
             // 
-            this.LoadButton.Location = new System.Drawing.Point(239, 143);
+            this.LoadButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.LoadButton.Location = new System.Drawing.Point(376, 144);
             this.LoadButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.LoadButton.Name = "LoadButton";
-            this.LoadButton.Size = new System.Drawing.Size(67, 33);
+            this.LoadButton.Size = new System.Drawing.Size(181, 33);
             this.LoadButton.TabIndex = 7;
             this.LoadButton.Text = "Load";
             this.LoadButton.UseVisualStyleBackColor = true;
@@ -128,32 +147,57 @@
             // 
             // NewFile
             // 
-            this.NewFile.Location = new System.Drawing.Point(93, 143);
+            this.NewFile.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.NewFile.ImageAlign = System.Drawing.ContentAlignment.TopRight;
+            this.NewFile.Location = new System.Drawing.Point(3, 144);
             this.NewFile.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.NewFile.Name = "NewFile";
-            this.NewFile.Size = new System.Drawing.Size(67, 33);
+            this.NewFile.Size = new System.Drawing.Size(179, 33);
             this.NewFile.TabIndex = 8;
             this.NewFile.Text = "New";
             this.NewFile.UseVisualStyleBackColor = true;
             this.NewFile.Click += new System.EventHandler(this.NewFile_Click);
             // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.AutoSize = true;
+            this.tableLayoutPanel1.ColumnCount = 4;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 24.77876F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25.17208F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25.02458F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25.02458F));
+            this.tableLayoutPanel1.Controls.Add(this.ExitButton, 3, 2);
+            this.tableLayoutPanel1.Controls.Add(this.LoadButton, 2, 2);
+            this.tableLayoutPanel1.Controls.Add(this.NewFile, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.SaveButton, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.textBox1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.ImportSelect, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.GenerateSelect, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.ModifySelect, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.DeleteSelect, 3, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 63.35878F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 36.64122F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(749, 201);
+            this.tableLayoutPanel1.TabIndex = 9;
+            // 
             // ActionSelect
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(469, 187);
-            this.Controls.Add(this.NewFile);
-            this.Controls.Add(this.LoadButton);
-            this.Controls.Add(this.SaveButton);
-            this.Controls.Add(this.DeleteSelect);
-            this.Controls.Add(this.ModifySelect);
-            this.Controls.Add(this.ExitButton);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.GenerateSelect);
-            this.Controls.Add(this.ImportSelect);
+            this.ClientSize = new System.Drawing.Size(749, 201);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "ActionSelect";
             this.Text = "ActionSelect";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -170,5 +214,6 @@
         private System.Windows.Forms.Button SaveButton;
         private System.Windows.Forms.Button LoadButton;
         private System.Windows.Forms.Button NewFile;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
     }
 }
