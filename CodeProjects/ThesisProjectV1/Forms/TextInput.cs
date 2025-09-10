634bfc6c9f849af703330f572d2c96c879ae95f1
corbin.ferguson <corbin.ferguson@endress.com>
2025-09-10 10:50:56 -0400

    regex default no whitespace

--- CodeProjects/ThesisProjectV1/Forms/TextInput.cs
+++ CodeProjects/ThesisProjectV1/Forms/TextInput.cs
@@ -13,7 +13,7 @@ namespace ThesisProjectV1.Forms
             InitializeComponent();
         }
 
-        public TextInput(string header, string defaultText = "", string reg = @"")
+        public TextInput(string header, string defaultText = "", string reg = @"\w")
         {
             InitializeComponent();
             UserInstructionTextBox.Text = header;
