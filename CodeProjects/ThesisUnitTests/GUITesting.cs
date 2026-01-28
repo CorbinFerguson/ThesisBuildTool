e60b118ba7a458c56fb13f642340635c7cf5b804
Corbin Ferguson <corbin.ferguson@endress.com>
2026-01-28 12:48:34 -0500

    switch to FlaUI.Core

--- CodeProjects/ThesisUnitTests/GUITesting.cs
+++ CodeProjects/ThesisUnitTests/GUITesting.cs
@@ -1,6 +1,9 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using FlaUI.Core;
+using FlaUI.UIA2;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Threading;
 
@@ -10,45 +13,29 @@ namespace ThesisUnitTests
     public class GUITesting
     {
         private readonly Uri WindowsApplicationDriverUrl = new Uri("http://127.0.0.1:4723");
-        private readonly string TestAppPath = @"C:\Users\i01505732\OneDrive - Endress+Hauser\Thesis Work\CodeProjects\ThesisProjectV1\bin\Debug\ThesisProjectV1.exe";
-       public Process appProc;
-        private TimeSpan timeoutInMS = TimeSpan.FromMilliseconds(10000);
+        private readonly string TestAppPath = typeof(ThesisProjectV1.Program).Assembly.Location;
+        public Application app;
 
         [TestInitialize]
         public void Startup()
         {
-
-            appProc = Process.GetProcessesByName("ThesisProjectV1").FirstOrDefault();
-            if (appProc == null)
-            {
-                appProc = Process.Start(TestAppPath);
-                Assert.IsNotNull(appProc);
-            }
-
-            // Check that ActionSelect launches
-            string hexHandle = null;
-            Stopwatch elapsed = Stopwatch.StartNew();
-            while (hexHandle == null && elapsed.Elapsed < timeoutInMS)
-            {
-                IntPtr mwhndl = appProc.MainWindowHandle;
-                hexHandle = mwhndl.ToString("x");
-                Thread.Sleep(25);
-            }
-            elapsed.Stop();
-
-            Assert.IsTrue(elapsed.Elapsed < timeoutInMS, "Timed out getting ActionSelect");
+            app = FlaUI.Core.Application.Launch(TestAppPath);
         }
 
         [TestCleanup]
         public void Teardown()
         {
-            appProc?.Close();
+            app?.Close();
         }
 
         [TestMethod]
         public void ActionSelectLayout()
         {
-            Assert.IsNull(appProc);
+            using (var automation = new UIA2Automation())
+            {
+                var actionSelect = app.GetAllTopLevelWindows(automation);
+            }
+            Assert.IsNull(app);
         }
     }
 }
